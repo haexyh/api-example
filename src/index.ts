854e3f8ecbb895d3c40f357c83baaf324c41fbbb
Pascal Klaiss <klaiss@gmx.ch>
2021-10-06 12:58:10 +0200

    changes on index.ts

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,6 @@ import {Food} from "./Dto/food";
 const app = express();
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-    res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
     next();
 });
 const port = 8080; // default port to listen
